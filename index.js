a97bc986d0254128a528ba7b4e12846e7f232e3c
AnhNT <ah.ngotuananh122001@gmail.com>
2022-08-23 06:40:00 +0000

    abc

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const express = require('express')
-const { ApolloServer } = require('apollo-server-express')
-const http = require('http')
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} = require('apollo-server-core');
 
-const typeDefs = require('./schema/schema')
-const resolvers = require('./resolver/resolver')
+const typeDefs = require('./schema/schema');
+const resolvers = require('./resolver/resolver');
 
-async function startApolloServer(typeDefs, resolvers) {
+const http = require('http');
+
+async function startApolloServer() {
   const app = express();
-
+  // Our httpServer handles incoming requests to our Express app.
+  // Below, we tell Apollo Server to "drain" this httpServer,
+  // enabling our servers to shut down gracefully.
   const httpServer = http.createServer(app);
-
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
   });
 
   await server.start();
 
+  // Mount Apollo middleware here.
   server.applyMiddleware({ app });
-
   await new Promise(resolve => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
-
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
 }
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer();
