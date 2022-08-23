cee632bd6c74fd12136bdd8d08cd822075e96f4f
AnhNT <ah.ngotuananh122001@gmail.com>
2022-08-23 06:51:10 +0000

    adaf

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageLocalDefault,
+    ApolloServerPluginDrainHttpServer,
+    ApolloServerPluginLandingPageLocalDefault,
 } = require('apollo-server-core');
 
 const typeDefs = require('./schema/schema');
@@ -11,27 +11,34 @@ const resolvers = require('./resolver/resolver');
 const http = require('http');
 
 async function startApolloServer() {
-  const app = express();
-  // Our httpServer handles incoming requests to our Express app.
-  // Below, we tell Apollo Server to "drain" this httpServer,
-  // enabling our servers to shut down gracefully.
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      //ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
-  });
+    const app = express();
+    // Our httpServer handles incoming requests to our Express app.
+    // Below, we tell Apollo Server to "drain" this httpServer,
+    // enabling our servers to shut down gracefully.
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [
+            ApolloServerPluginLandingPageProductionDefault({
+                graphRef: 'my-graph-id@my-graph-variant',
+                footer: false,
+            }),
+            //ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+        ],
+    });
 
-  await server.start();
+    await server.start();
 
-  // Mount Apollo middleware here.
-  server.applyMiddleware({ app });
-  await new Promise(resolve => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-  return { server, app };
+    // Mount Apollo middleware here.
+    server.applyMiddleware({ app });
+    await new Promise((resolve) =>
+        httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
+    );
+    console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+    );
+    return { server, app };
 }
 
 startApolloServer();
